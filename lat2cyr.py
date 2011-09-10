852268f6ca664f40138faafea466bc0f4bd3e66d
Scroll Lock <scroll_lock@abv.bg>
2011-09-09 23:07:30 -0400

    final touches

--- lat2cyr.py
+++ lat2cyr.py
@@ -1,15 +1,35 @@
-import os
-import string
+#!/usr/bin/python
+# -*- coding: iso-8859-5 -*-
+
 from __future__ import with_statement
+import os
+import codecs
+import optparse
+from string import maketrans
 
-def lat2cyr(input, output):
-    in = ''
-    if os.path.exists(input):
-        with file.open(input)
-        
+def lat2cyr(inputText, output):
+    i = 'abcdefghijklmnopqrstuvwxyz`[]\\ABCDEFGHIJKLMNOPQRSTUVWXYZ~{}|'
+    o = 'ĞÑæÔÕäÓåØÙÚÛÜİŞßïàáâãÖÒìê×çèéî°±Æ´µÄ³Å¸¹º»¼½¾¿ÏÀÁÂÃ¶²ÊÊ·ÇÈÉÎ'
+    table = maketrans(i,o)
     
-    
+    result = ''
+    if os.path.exists(inputText):
+        with open(inputText) as f:
+            inputText = f.read()
 
+    for char in inputText:
+        if char in i:
+            result = result + char.translate(table)
+
+        else :
+            result = result + char
+
+    if output:
+        with codecs.open(output, 'w', 'utf-8') as f:
+            f.write(result.decode('iso-8859-5'))
+    else:
+        print result.decode('iso-8859-5')
+                
 
 def main():
     '''
@@ -23,9 +43,6 @@ def main():
         p.error('Neither input string or file given. Use -i flag.')
 
     lat2cyr(opt.input, opt.output)
-
-
-
 
 
 if __name__ == '__main__':
